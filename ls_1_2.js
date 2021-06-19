042f26c66895c05de257210cad54eb16a564dc12
k_yelaman <k.elaman2701@gmail.com>
2021-06-19 22:33:13 +0600

    add first lesson homeworks files

---
+++ ls_1_2.js
@@ -0,0 +1,9 @@
+var name = 'Еламан'
+var age = 21
+var hobby = 'играть в видеоигры'
+var forConcatenation = 'Работа'
+var forConcatenation2 = 'переменными'
+
+console.log(`Привет! Давайте познакомимся. Меня зовут ${name}. Мне ${age}. Моё любимое занятие это ${hobby}.`)
+console.log("На первом уроке от Geekbrains по js я научился делать конкатенацию и многое другое.")
+console.log("Эта надпись сделана при помощи конкатенации. " + forConcatenation + ' c ' + forConcatenation2 + '.')
---
+++ ls_1_3.js
@@ -0,0 +1,4 @@
+var admin = ''
+var name = "Василий"
+admin = name
+console.log(`В перемнной admin имя: ${admin}`)
